a448d5d445716d4418bea216ec41ab369e2439e3
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2024-12-26 13:19:07 +0530

    Redundant files removed

--- json_flatten.py
+++
@@ -1,7 +0,0 @@
-import json
-
-jsonFilePath = r'assets/buildings.json'
-with open(jsonFilePath,'r') as jsonToFlatten:
-    jsonDict = json.loads(jsonToFlatten.read())
-    flattenedJson = json.dumps(jsonDict['buildings'][0]['GroundFloor'])
-    print(flattenedJson)
